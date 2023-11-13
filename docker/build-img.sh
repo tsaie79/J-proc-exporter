1456999701d6e9029904c8340f0007e64a271f25
tsaie79 <tsaie79@gmail.com>
2023-11-13 15:32:47 -0500

    Update docker build script to tag and push image with version number

--- docker/build-img.sh
+++ docker/build-img.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t jlabtsai/process-exporter:v1.0 --no-cache .
+docker build -t jlabtsai/process-exporter:v1.0.0 --no-cache .
+docker push jlabtsai/process-exporter:v1.0.0
