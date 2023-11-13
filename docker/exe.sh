9aeb3bbd58c15fe66b051cc693fe0d37e50ef2ff
tsaie79 <tsaie79@gmail.com>
2023-11-13 11:50:34 -0500

    Add repeat launch and kill process functionality to exe.sh

--- docker/exe.sh
+++ docker/exe.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/get_cmds.sh
-/bin/process-exporter --procfs /host_proc --config.path $HOME/config.yml --web.listen-address=:$PROCESS_EXPORTER_PORT
+#repeat launch every 3 seconds and kill the launch process
+while true; do
+    setsid /run-exporter.sh& echo $! > $HOME/launch-exporter.sid
+    sleep 10
+    kill -9 `pgrep -s $(cat $HOME/launch-exporter.sid)`
+done
+
+
+## This has a problem that when the process-exporter is killed, the ssh tunnel will fail
