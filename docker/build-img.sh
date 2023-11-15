2f46eddb31c7d8f25a2639a005b7df9c9461343c
tsaie79 <tsaie79@gmail.com>
2023-11-15 15:46:07 -0500

    Update process-exporter image version to v1.0.1

--- docker/build-img.sh
+++ docker/build-img.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t jlabtsai/process-exporter:v1.0.0 --no-cache .
-docker push jlabtsai/process-exporter:v1.0.0
+docker build -t jlabtsai/process-exporter:v1.0.1 --no-cache .
+docker push jlabtsai/process-exporter:v1.0.1
