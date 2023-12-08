8eb92db45d981b86102dfe8b9c16cddeddac8ff3
tsaie79 <tsaie79@gmail.com>
2023-12-08 01:53:23 -0500

    Update process exporter configuration file

--- docker/get_cmds.sh
+++ docker/get_cmds.sh
@@ -3,13 +3,13 @@
 # This give a list of pids that are in the container. I want to loop get the result of the cmd
 #cat /proc/pid/stat | awk '{print $2}' and remove the perenthesis
 
-cp /config.yml $HOME/config.yml
+cp /config.yml $PROCESS_EXPORTER_CONFIG
 
-for pid in $(pgrep -g `cat $1`);
+for pid in $(pgrep -g `cat $PGID_FILE`);
 do
     export cmd=$(cat /proc/$pid/stat | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
     echo $cmd
 
-    sed -i "s/comm:/&\n  - \"$cmd\"/" $HOME/config.yml
+    sed -i "s/comm:/&\n  - \"$cmd\"/" $PROCESS_EXPORTER_CONFIG
 done
 
